3f4cb6c9539c20403c7a1167e7ea7bfa113f5a76
Luigy Leon <luigy@outlook.com>
2017-04-24 14:01:36 -0400

    Make creation of date picker lazy and move visibility toggling outside of create ffi.

--- src/Reflex/Dom/DHTMLX/Date.hs
+++ src/Reflex/Dom/DHTMLX/Date.hs
@@ -10,6 +10,7 @@ module Reflex.Dom.DHTMLX.Date where
 
 ------------------------------------------------------------------------------
 import           Control.Lens
+import           Control.Monad
 import           Control.Monad.Trans
 import           Data.Default
 import           Data.Map                (Map)
@@ -21,6 +22,8 @@ import           GHCJS.DOM.Types hiding (Event, Text)
 import           GHCJS.Marshal.Pure (pToJSVal)
 import           GHCJS.Foreign.Callback
 import           GHCJS.Types
+import qualified GHCJS.DOM.Element as Element
+import           GHCJS.DOM.EventM (on)
 #endif
 import           Reflex.Dom hiding (Element, fromJSString)
 import           Reflex.Dom.DHTMLX.Common
@@ -33,7 +36,7 @@ data DateWidgetRef
 #endif
 
 ------------------------------------------------------------------------------
-createDhtmlxDateWidget :: Element -> WeekDay -> Bool -> IO DateWidgetRef
+createDhtmlxDateWidget :: Element -> WeekDay -> IO DateWidgetRef
 #ifdef ghcjs_HOST_OS
 createDhtmlxDateWidget elmt wstart =
     js_createDhtmlxDateWidget (pToJSVal elmt) (weekDayToInt wstart)
@@ -43,12 +46,9 @@ foreign import javascript unsafe
     var cal = new dhtmlXCalendarObject($1);\
     cal.setWeekStartDay($2);\
     cal.hideTime();\
-    if ($3) {\
-      cal.show();\
-    }\
     return cal;\
    })()"
-  js_createDhtmlxDateWidget :: JSVal -> Int -> Bool -> IO DateWidgetRef
+  js_createDhtmlxDateWidget :: JSVal -> Int -> IO DateWidgetRef
 
 #else
 createDhtmlxDateWidget = error "createDhtmlxDateWidget: can only be used with GHCJS"
@@ -60,7 +60,6 @@ createDhtmlxDateWidgetButton
     :: Element
     -> Element
     -> WeekDay
-    -> Bool
     -> IO DateWidgetRef
 #ifdef ghcjs_HOST_OS
 createDhtmlxDateWidgetButton b elmt wstart =
@@ -72,12 +71,9 @@ foreign import javascript unsafe
     var cal = new dhtmlXCalendarObject({input: $2, button: $1});\
     cal.setWeekStartDay($3);\
     cal.hideTime();\
-    if ($3) {\
-      cal.show();\
-    }\
     return cal;\
    })()"
-  js_createDhtmlxDateWidgetButton :: JSVal -> JSVal -> Int -> Bool -> IO DateWidgetRef
+  js_createDhtmlxDateWidgetButton :: JSVal -> JSVal -> Int -> IO DateWidgetRef
 
 #else
 createDhtmlxDateWidgetButton =
@@ -97,6 +93,17 @@ foreign import javascript unsafe
 getDateWidgetValue = error "getDateWidgetValue: can only be used with GHCJS"
 #endif
 
+dateWidgetShow :: MonadIO m  => DateWidgetRef -> m ()
+#ifdef ghcjs_HOST_OS
+dateWidgetShow = liftIO . js_dateWidgetShow
+
+foreign import javascript unsafe
+  "(function(){ $1['show'](); })()"
+  js_dateWidgetShow :: DateWidgetRef -> IO ()
+
+#else
+dateWidgetShow _ = return ()
+#endif
 
 ------------------------------------------------------------------------------
 dateWidgetUpdates
@@ -158,9 +165,27 @@ dhtmlxDatePicker (DatePickerConfig iv sv b wstart attrs visibleOnLoad) = do
       & textInputConfig_initialValue .~ formatter iv
       & textInputConfig_setValue .~ fmap formatter sv
     let dateEl = toElement $ _textInput_element ti
-    let create = maybe createDhtmlxDateWidget createDhtmlxDateWidgetButton b
-    calRef <- liftIO $ create dateEl wstart visibleOnLoad
-    ups <- dateWidgetUpdates calRef
+    let create p f = do
+          calRef <- liftIO $ f dateEl wstart
+          when p $ dateWidgetShow calRef
+          dateWidgetUpdates calRef
+    ups <- case b of
+      Nothing | visibleOnLoad -> create True createDhtmlxDateWidget
+      Nothing -> do
+        lazyCreate <- headE $ create False createDhtmlxDateWidget <$ domEvent Focus ti
+        fmap (switch . current) $ widgetHold (return never) lazyCreate
+      Just b' | visibleOnLoad -> create True $ createDhtmlxDateWidgetButton b'
+      Just b' -> do
+#ifdef ghcjs_HOST_OS
+        click' <- wrapDomEvent b' (`on` Element.click) $ return ()
+#else
+        let click' = never
+#endif
+        lazyCreate <- headE $ leftmost
+          [ create False (createDhtmlxDateWidgetButton b') <$ domEvent Focus ti
+          , create True (createDhtmlxDateWidgetButton b') <$ click'
+          ]
+        fmap (switch . current) $ widgetHold (return never) lazyCreate
     let parser = parseTimeM True defaultTimeLocale fmt . T.unpack
     fmap DatePicker $ holdDyn iv $ leftmost
       [ parser <$> _textInput_input ti
